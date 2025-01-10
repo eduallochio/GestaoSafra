aa9d2e8994247c70f8d8161f1a507361f60a782f
Eduardo Allochio <eduallochio2@outlook.com>
2025-01-10 14:49:59 -0300

    landing page Gestao Safra

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Script para rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Script para o botão de voltar ao topo
+const backToTopButton = document.querySelector('.back-to-top');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
