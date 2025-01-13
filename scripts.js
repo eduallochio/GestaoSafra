c30a0f90b0e5ea398d6c2a0f76c6509467ad8d8c
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2025-01-12 23:11:34 -0300

    carousel

--- scripts.js
+++ scripts.js
@@ -1,28 +1,51 @@
 // Script para rolagem suave
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault()
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    })
+  })
+})
 
 // Script para o botão de voltar ao topo
-const backToTopButton = document.querySelector('.back-to-top');
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        backToTopButton.classList.add('show');
-    } else {
-        backToTopButton.classList.remove('show');
-    }
-});
+const backToTopButton = document.querySelector(".back-to-top")
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("show")
+  } else {
+    backToTopButton.classList.remove("show")
+  }
+})
 
 // Adiciona evento de clique ao botão de voltar ao topo
-backToTopButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+backToTopButton.addEventListener("click", (e) => {
+  e.preventDefault()
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+})
+
+// Script para o carrossel
+document.querySelectorAll(".carousel").forEach((carousel) => {
+  let currentIndex = 0
+  const items = carousel.querySelectorAll(".carousel-item")
+  const totalItems = items.length
+
+  carousel
+    .querySelector(".carousel-control-next")
+    .addEventListener("click", () => {
+      items[currentIndex].classList.remove("active")
+      currentIndex = (currentIndex + 1) % totalItems
+      items[currentIndex].classList.add("active")
+    })
+
+  carousel
+    .querySelector(".carousel-control-prev")
+    .addEventListener("click", () => {
+      items[currentIndex].classList.remove("active")
+      currentIndex = (currentIndex - 1 + totalItems) % totalItems
+      items[currentIndex].classList.add("active")
+    })
+})
